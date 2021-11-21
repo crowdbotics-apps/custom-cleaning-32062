a6f38b7d058936878a7bb1d02235382a03687b39
Crowdbotics <team@crowdbotics.com>
2021-11-21 00:51:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "custom_cleaning_32062",
+  url: "https://custom_cleaning_32062.botics.co",
+  api: "https://custom_cleaning_32062.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
